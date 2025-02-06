a485ec7329b3eddc5b53f83b955515a156da7774
Beridze0 <beridzedavit904@gmail.com>
2025-02-07 00:06:23 +0400

    added orderSummary localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
-import MainContent from './components/MainContent/MainContent'
-import { Route, Routes } from 'react-router-dom'
-import Header from './components/Header/Header'
-import SearchBar from './components/MainContent/SearchBar/SearchBar'
-import generateRandomLetter from './utils/letters'
-import useMealsFetch from './hooks/useMealsFetch'
+import React, { useEffect, useState } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import MainContent from "./components/MainContent/MainContent";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import SearchBar from "./components/MainContent/SearchBar/SearchBar";
+import generateRandomLetter from "./utils/letters";
+import useMealsFetch from "./hooks/useMealsFetch";
 
 const App = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [categoryValue, setCategoryValue] = useState();
+  const [mealDetails, setMealDetails] = useState(JSON.parse(localStorage.getItem('meal-details')) || []);
 
-  const [searchInput, setSearchInput] = useState('')
-  const [categoryValue, setCategoryValue] = useState()
-  const [mealDetails, setMealDetails] = useState([])
+  const mealsData = useMealsFetch(
+    searchInput,
+    categoryValue,
+    generateRandomLetter
+  );
 
-  const mealsData = useMealsFetch(searchInput, categoryValue, generateRandomLetter)
+  const getSearchAndCategory = (value) => {
+    setSearchInput(value);
+    setCategoryValue(value);
+  };
 
-  const getSearchAndCategory = (value) =>{
-    setSearchInput(value)
-    setCategoryValue(value)
-  }
+  const getMealsDetails = (meal) => {
+    setMealDetails(prev => {
+      const prevMeal = [...prev]
+      console.log(meal);
+      
+      localStorage.setItem('meal-details', JSON.stringify([...prevMeal, meal]))
+      return [...prevMeal, meal]
+    })
+  };
 
-
-  const getMealsDetails = (meal) =>{
-      setMealDetails(prev => {
-        localStorage.setItem('ordered-meal', JSON.stringify([...prev, meal]))
-        return [...prev, meal]
-      })
-
-  }
-  
-  
-
-  
   return (
-    <div className='app'>
+    <div className="app">
       <Sidebar />
-      <div className='app-main-content'>
-        
-          <Header />
-          <SearchBar getSearchAndCategory={getSearchAndCategory} />
+      <div className="app-main-content">
+        <Header />
+        <SearchBar getSearchAndCategory={getSearchAndCategory} />
         <Routes>
           <Route
             path="/"
-            element={<MainContent
-                      mealsData={mealsData}
-                      getMealsDetails={getMealsDetails} 
-                      mealDetails={mealDetails}
-                      />} 
+            element={
+              <MainContent
+                mealsData={mealsData}
+                getMealsDetails={getMealsDetails}
+                mealDetails={mealDetails}
+              />
+            }
           />
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/MainContent/OrderedItem/OrderedItem.jsx
+++ src/components/MainContent/OrderedItem/OrderedItem.jsx
@@ -1,51 +1,68 @@
-import React, { useState } from 'react'
-import './OrderedItem.css'
+import React, { useEffect, useState } from "react";
+import "./OrderedItem.css";
 import { FiEdit3 } from "react-icons/fi";
 import { RiDeleteBin5Line } from "react-icons/ri";
 import { MdOutlineEditOff } from "react-icons/md";
 
-const OrderedItem = ({mealDetails}) => {
+const OrderedItem = ({ mealDetails }) => {
+  const [editWindow, setEditWindow] = useState(false);
+  const [quantity, setQuantity] = useState(JSON.parse(localStorage.getItem(`quantity-${mealDetails?.idMeal}`)) || 1);
 
-    const [editWindow, setEditWindow] = useState(false)
-    const [quantity, setQuantity] = useState(1)
+  useEffect(() => {
+    localStorage.setItem(`quantity-${mealDetails?.idMeal}`, quantity);
+}, [quantity, mealDetails?.idMeal]);
 
   return (
-    <div className='order-items'>
-                <div className='ordered-food'>
-                    <div className='ordered-food-img-container'>
-                        <img src={mealDetails?.strMealThumb} className='ordered-food-img' />
-                    </div>
-                    
-                    <div className='ordered-food-details'>
-                        <div className='ordered-food-title'>
-                            <p>{mealDetails?.strMeal}</p>
-                            <p>Quantity: {quantity}</p>
-                        </div>
-                        <div className='ordered-food-price'>
-                            <p>$13.00</p>
-                            <div className='ordered-food-edit'>
-                                {   editWindow?
-                                    <MdOutlineEditOff className='order-edit' onClick={()=> setEditWindow(prev => !prev)} /> :
-                                    <FiEdit3 className='order-edit' onClick={()=> setEditWindow(prev => !prev)}  />
-                                }
-                                    {
-                                        editWindow && 
-                                        <div className='edit-window'>
-                                           <p>QUANTITY</p> 
-                                           <div className='quantity-btn'>
-                                                <button onClick={() => setQuantity(prev => Math.max(1, prev - 1))}>-</button>
-                                                <p>{quantity > 0? quantity : 1}</p>
-                                                <button onClick={() => setQuantity(prev => prev + 1)}>+</button>
-                                           </div>
-                                        </div>
-                                    }
-                                <RiDeleteBin5Line className='order-delete' />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-  )
-}
+    <div className="order-items">
+      <div className="ordered-food">
+        <div className="ordered-food-img-container">
+          <img src={mealDetails?.strMealThumb} className="ordered-food-img" />
+        </div>
 
-export default OrderedItem
+        <div className="ordered-food-details">
+          <div className="ordered-food-title">
+            <p>{mealDetails?.strMeal}</p>
+            <p>Quantity: {quantity}</p>
+          </div>
+          <div className="ordered-food-price">
+            <p>$13.00</p>
+            <div className="ordered-food-edit">
+              {editWindow ? (
+                <MdOutlineEditOff
+                  className="order-edit"
+                  onClick={() => setEditWindow((prev) => !prev)}
+                />
+              ) : (
+                <FiEdit3
+                  className="order-edit"
+                  onClick={() => setEditWindow((prev) => !prev)}
+                />
+              )}
+              {editWindow && (
+                <div className="edit-window">
+                  <p>QUANTITY</p>
+                  <div className="quantity-btn">
+                    <button
+                      onClick={() =>
+                        setQuantity((prev) => Math.max(1, prev - 1))
+                      }
+                    >
+                      -
+                    </button>
+                    <p>{quantity > 0 ? quantity : 1}</p>
+                    <button onClick={() => setQuantity((prev) => prev + 1)}>
+                      +
+                    </button>
+                  </div>
+                </div>
+              )}
+              <RiDeleteBin5Line className="order-delete" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderedItem;
